d26dc48221a8dd6e6120aecf08773cfde9f9838a
k-iatrou <158864273+k-iatrou@users.noreply.github.com>
2024-02-20 12:06:52 +0100

    increased the age by 4 years, calculated the new insurance cost, calculated the change in insurance cost

--- scrypt.py
+++ scrypt.py
@@ -9,3 +9,13 @@ smoker = 0
 insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 
 print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
+
+age += 4
+
+# calculate new insurance cost
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+# calculate change in insurance cost
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in cost of insurance after increasing the age by 4 years is " + str(change_in_insurance_cost) + " dollars.")
