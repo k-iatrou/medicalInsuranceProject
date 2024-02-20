55e3f3ceba7a9de6697139008573a3693d8a3065
k-iatrou <158864273+k-iatrou@users.noreply.github.com>
2024-02-20 11:51:02 +0100

    var with calculation for insurance cost

--- scrypt.py
+++ scrypt.py
@@ -1,6 +1,11 @@
 # person data
 age = 28
-sex = "female"
+sex = 0
 bmi = 26.2
 num_of_children = 3
-smoker = "non-smoker"
+smoker = 0
+
+# calculating insurance cost
+insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
